e178ebcbf80d981f4719ba6c68eedb6706a70f4b
Amit Agarwal <60168762+aamit2267@users.noreply.github.com>
2021-01-14 11:02:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,22 @@
-from cryptography.fernet import Fernet
-
-
+print("Enter text to encrypt:")
+text=(input())
 
 def encrypt_text(text):
-    
-    key=Fernet.generate_key()
-    
-    print("Your secret key is:",key.decode('ascii'))
-   
-    b = bytes(text, 'utf-8')
-    
-    f=Fernet(key)
-    
-    print()
-    
-    token=f.encrypt(b)
-
-    print("Your encrypted text is:",token.decode('ascii'))
-   
-def decrypt_text(key,text):
-    k=bytes(key, 'utf-8')
-    b = bytes(text, 'utf-8')
-    f=Fernet(k)
-    
-    dec=f.decrypt(b)
-
-    print("Your decrypted text is:",dec.decode('ascii'))
-    
-    
-print("Enter text:")
-text=(input())
-print()
-print("For encryption,press 1.\nFor decryption press 2:")
-enorde=int(input())
-
-
-if enorde==1:
-    encrypt_text(text)
-else:
-    print("Enter Secret Key:")
-    secret_key=input()
-    print()
-    decrypt_text(secret_key,text)
+	encoded_text = '-'
+	lister = []
+	alpha = 'a'
+	alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+	all_numbers = [1,2,3,4,5,6,7,8,9]
+	for letter in text:
+		if letter in str(all_numbers):
+			ind = str(alphabets[int(letter)-1])
+			lister.append(ind)
+		else:
+			lowered_case = letter.lower()
+			if lowered_case in alphabets:
+				ind = alphabets.index(lowered_case)
+				lister.append(str(ind+1))
+	return encoded_text.join(lister)
+	
+print(encrypt_text(text))
+	
